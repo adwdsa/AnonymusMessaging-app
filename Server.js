d414e0bb5cf698a2b7f15b78a0f0d3b2fd070e4e
adwdsa <120736423+adwdsa@users.noreply.github.com>
2024-12-20 23:05:49 +0400

    Update Server.js

--- Server.js
+++ Server.js
@@ -22,7 +22,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 80;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
